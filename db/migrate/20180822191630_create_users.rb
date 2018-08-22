2970efb0b701d9ce9e1834ad9d571fe03c0b368d
car <carmelogustavo@gmail.com>
2018-08-22 21:40:26 +0200

    create and migrate db

--- db/migrate/20180822191630_create_users.rb
+++ db/migrate/20180822191630_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.int :password
+      t.integer :password
       t.date :birthday
 
       t.timestamps
